54765d92c5077b3fff6586b8cba125b27a5c528f
Justin Tan <tanjustin217@gmail.com>
2024-08-07 11:39:06 -0600

    Implemented function to change grid size
    Prompt user and then utilized the total area and total side length to calculate side lengths of individual boxes
    Reworked createBox function to account for varied box side length

--- script.js
+++ script.js
@@ -8,30 +8,42 @@ changeGridButton.addEventListener("click", (e) => {
 });
 
 function changeGrid() {
-  //let sidelength = prompt("How many squares would you like on each edge?");
-  container.childNodes.forEach((element) => {
-    container.remove(container.childNodes);
-  });
+  let sideNum = parseInt(
+    prompt("How many squares would you like on each edge?")
+  );
+  if (parseInt(sideNum) <= 100) {
+    let child = container.lastElementChild;
+    while (child) {
+      container.removeChild(child);
+      child = container.lastElementChild;
+    }
+
+    let newBoxLength = 800 / sideNum;
+    createBoxes(newBoxLength);
+  }
 }
 
-function createBoxes(number) {
-  for (i = 0; i < number; i++) {
+function createBoxes(length) {
+  let totalBoxes = 640000 / length ** 2;
+  for (i = 0; i < totalBoxes; i++) {
     container.appendChild(document.createElement("div"));
   }
   container.childNodes.forEach((element) => {
+    element.style.width = `${length}px`;
+    element.style.height = `${length}px`;
     element.classList.add("blankBox");
     element.addEventListener("mouseover", () => {
       element.classList.add("hoverBlackBox");
     });
     element.addEventListener("mouseleave", () => {
-      element.classList.add("blankBox");
       element.classList.remove("hoverBlackBox");
     });
     element.addEventListener("click", () => {
+      element.classList.remove("blankBox");
       element.classList.add("permBlackBox");
       element.style.opacity = "100%";
     });
   });
 }
 
-createBoxes(256);
+createBoxes(50);
