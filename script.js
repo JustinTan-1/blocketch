98b64a1e2fcc6ec155fdc2948c1c25c9a442b75c
Justin Tan <tanjustin217@gmail.com>
2024-08-07 09:01:06 -0600

    Implement square event listener
    Changes the color of the square when mouse hovers over the blocks

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function createBoxes(number) {
   for (i = 0; i < number; i++) {
     container.appendChild(document.createElement("div"));
   }
+  const testArray = container.childNodes;
+  console.log(testArray);
   container.childNodes.forEach((element) => {
     element.classList.add("box");
+    element.addEventListener("mouseover", () => {
+      element.style.backgroundColor = "black";
+    });
   });
 }
 
