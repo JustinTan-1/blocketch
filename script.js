c3eb019fe9cc6c3b3eb6b7ebe14cef98ebf1731e
Justin Tan <tanjustin217@gmail.com>
2024-08-07 09:45:19 -0600

    Fix deletion bug
    Using the forEach loop caused only half of the nodes to be deleted
    Simply removing all the child nodes at once fixes this problem

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ changeGridButton.addEventListener("click", (e) => {
 function changeGrid() {
   //let sidelength = prompt("How many squares would you like on each edge?");
   container.childNodes.forEach((element) => {
-    console.log(element);
-    container.removeChild(element);
+    container.remove(container.childNodes);
   });
 }
 
@@ -19,8 +18,6 @@ function createBoxes(number) {
   for (i = 0; i < number; i++) {
     container.appendChild(document.createElement("div"));
   }
-  const testArray = container.childNodes;
-  console.log(testArray);
   container.childNodes.forEach((element) => {
     element.classList.add("box");
     element.addEventListener("mouseover", () => {
