7e96d1bdb37479750205ecb048161aa66ff3a6d5
Justin Tan <tanjustin217@gmail.com>
2024-08-07 09:30:15 -0600

    Add callback function to button event listener
    Function would fire after page reload. Callback function checks for target = button

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const container = document.querySelector(".container");
+const changeGridButton = document.querySelector(".changeGrid");
+
+changeGridButton.addEventListener("click", (e) => {
+  if (e.target.classList == "changeGrid") {
+    changeGrid();
+  }
+});
+
+function changeGrid() {
+  //let sidelength = prompt("How many squares would you like on each edge?");
+  container.childNodes.forEach((element) => {
+    console.log(element);
+    container.removeChild(element);
+  });
+}
 
 function createBoxes(number) {
   for (i = 0; i < number; i++) {
