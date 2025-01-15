2b398dd25f19d0051395052a18cad8188f0136ae
Justin Tan <tanjustin217@gmail.com>
2025-01-15 10:18:08 -0700

    Save to database function

--- client/src/pages/Draw.jsx
+++ client/src/pages/Draw.jsx
@@ -3,12 +3,19 @@ import Pixel from "../components/Pixel"
 import { nanoid } from 'nanoid'
 import Color from "../components/Color"
 import Header from "../components/Header"
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 function App() {
   const [color, setColor] = React.useState("white")
   const [gridSize, setGridSize] = React.useState(15)
   const [grid, setGrid] = React.useState(generateArray(gridSize)) 
   const [userGridSize, setUserGridSize] = React.useState(15)
+  const [cookies, setCookie] = useCookies(['user'])
+  const [title, setTitle] = React.useState("")
+
+  const navigate = useNavigate()
 
 
 function paintBox(id) {
@@ -55,11 +62,32 @@ function handleChange(e) {
   setUserGridSize(value)
 }
 
+function handleSave(e){
+  e.preventDefault()
+  if (!cookies['user']) {
+    navigate("/login")
+  }
+  axios.post(
+    "http://localhost:8080/api/save", {
+      username: cookies['user'],
+      grid: grid,
+      name: title
+    }
+  )
+  .then((res) => {
+    if (res.data.error){
+      alert(res.data.error)
+  } else {
+      alert(res.data.success)
+  }
+  })
+}
+
   const diceArray = grid.map((element) => {
     return(<Pixel key={element.id} size = {gridSize} color={element.color} paintBox={() => paintBox(element.id)}></Pixel>)
   })  
 
-
+  console.log(grid)
   return (
     <>
     <Header></Header>
@@ -68,6 +96,10 @@ function handleChange(e) {
     <form onSubmit={handleSubmit}>
     <input placeholder="grid size" value={userGridSize} onChange={handleChange}></input>
     <button type="submit">Change Grid Size</button>
+    </form>
+    <form onSubmit={handleSave}>
+    <input placeholder="Painting Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+    <button type="submit">Save</button>
     </form>
     <div className="grid" style={{display: "grid", gridTemplateColumns: `repeat(${gridSize}, ${50/gridSize}vw)`, gridTemplateRows: `repeat(${gridSize}, ${50/gridSize}vw)`}}>{diceArray}</div>
     </>
--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import psycopg2
 from werkzeug.security import check_password_hash, generate_password_hash
 from psycopg2.errors import UniqueViolation
+import json
 
 
 
@@ -10,6 +11,31 @@ app = Flask(__name__)
 cors = CORS(app, origins="*")
 conn = psycopg2.connect(database="users", port="5432")
 cur = conn.cursor()
+
+@app.route("/api/save", methods=["POST"])
+def save():
+    if request.method == "POST":
+        data = request.get_json()
+        username = data.get("username")
+        grid = json.dumps(data.get("grid"))
+        name = data.get("name")
+        cur.execute("SELECT id FROM users WHERE username = %s", (username,))
+        user_id = cur.fetchone()[0]
+        cur.execute("SELECT * FROM paintings WHERE userid=%s AND name=%s",(user_id,name))
+        if not cur.fetchone():
+            try:
+                cur.execute("INSERT INTO paintings (grid, userid, name ) VALUES (%s,%s,%s)", (grid, user_id, name))
+                conn.commit()
+                return jsonify({"error": "Save Success1"})
+            except UniqueViolation:
+                conn.rollback()
+        else:
+            cur.execute("UPDATE paintings SET grid=%s WHERE userid=%s AND name=%s", (grid, user_id, name))
+            conn.commit()
+            return jsonify({"error": "Save Success2"})
+        
+    #cur.execute("SELECT paintings FROM users")
+    #return f"123 {cur.fetchone()[0][0]}"
 
 @app.route("/api/register", methods=["POST"])
 def register():
@@ -42,6 +68,9 @@ def login():
             return jsonify({"error": "Please Enter a Valid Username"})
         if not password or " " in password:
             return jsonify({"error": "Please Enter a Valid Password"})
+        cur.execute("SELECT username FROM users WHERE username = %s", (username,))
+        if not cur.fetchone():
+            return jsonify({"error": "Could not find user"})
         cur.execute("SELECT hash FROM users WHERE username = %s", (username,))
         if not check_password_hash(cur.fetchone()[0], password):
             return jsonify({"error": "Wrong Password"})
